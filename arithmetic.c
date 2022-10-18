b7b6cc23b57a6518879aaf82d2d961b62409840b
excal.rs <excal.rs@protonmail.com>
2022-10-18 20:51:23 +0100

    Updated Files

--- arithmetic.c
+++ arithmetic.c
@@ -13,18 +13,23 @@ int main()
     */
     
     int num = 2;
-    int num2 = 4;
+    int num2 = 3;
 
     printf("num = %d \n", num );
     printf("num2 = %d \n\n",num2 );
 
     printf("Increments: \n");
-    printf("num++ = %d \n", num++);
-    printf("num2++ = %d \n\n", num2++);
+    printf("num++ = %d \n", ++num);     // this is shorthand for num = num+1
+    printf("num2++ = %d \n\n", ++num2); // this is shorthand for num = num+1
+
+    num = 2; //reassign values as they are modified above
+    num2 = 3; //reassign values as they are modified above
 
     printf("Division: \n");
     printf("num/2 = %d \n", num/2 );
-    printf("num2/2 = %d \n\n",num2/2 );
+    printf("*Truncated* num2/2 = %f \n", num2/2 ); // this will give the DIV value (truncated)
+    printf("num2/2 = %f \n\n", num2 / 2.0 );
+
     
     printf("Multiplication: \n");
     printf("num*2 = %d \n", num*2 );

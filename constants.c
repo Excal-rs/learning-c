4de5dd274af39aef71e30a6c89331531daa759ac
excal.rs <excal.rs@protonmail.com>
2022-10-19 21:38:29 +0100

    Updates - added constants.c

---
+++ constants.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+
+int main(){
+// to create a constant preseed the datatype with const
+	const float PI = 3.14; // it is convention to name constants in all caps
+	//PI = 1.11111; //running this would lead to a error
+	printf("%f", PI);
+
+
+}

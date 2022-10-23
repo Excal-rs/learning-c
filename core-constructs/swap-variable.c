d719ae4990c1a4b03c7cae5b9e65e188e9188bb6
excal.rs <excal.rs@protonmail.com>
2022-10-23 17:56:06 +0100

    updates

---
+++ core-constructs/swap-variable.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    char x[] = "Hello" ;
+    char y[] = "World";
+    // char x = "h" ;
+    // char y = "w";
+
+    printf("Before: \n");
+    printf("variable x is = %s \n", x);
+    printf("variable y is = %s \n", y);
+
+    // With third variable: (more readable does not work with strings)
+    // char temp;
+    // temp = x;
+    // x = y;
+    // y = temp;
+
+    //Without third variable: (doesn't work for strings):
+    // x = x + y;
+    // y = x - y;
+    // x = x - y;
+
+    // For strings:
+    char temp[20];
+    strcpy(temp, x);
+    strcpy(x, y);
+    strcpy(y, temp);
+
+    printf("\nAfter: \n");
+    printf("variable x is = %s \n", x);
+    printf("variable y is = %s \n", y);
+    return 0;
+}
